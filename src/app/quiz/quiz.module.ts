e983831bd7fb2b57675f6c0dd34e28e71feff3df
Amine BENLAMA <benlama.amine@gmail.com>
2023-07-09 13:04:04 +0200

    the module was simplified and a shared objects have been added through shared Module

--- src/app/quiz/quiz.module.ts
+++ src/app/quiz/quiz.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { QuizRoutingModule } from './quiz-routing.module';
 import { QuizMakerComponent } from './quiz-maker/quiz-maker.component';
 import { QuizQuestionComponent } from './quiz-question/quiz-question.component';
 import { QuizResultComponent } from './quiz-result/quiz-result.component';
 import { QuizResetterComponent } from './quiz-resetter/quiz-resetter.component';
-import { UnescapePipe } from 'src/app/shared/index';
 import { QuizQuestionListComponent } from './quiz-question-list/quiz-question-list.component';
 
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,11 +15,10 @@ import { QuizQuestionListComponent } from './quiz-question-list/quiz-question-li
     QuizQuestionComponent,
     QuizResultComponent,
     QuizResetterComponent,
-    UnescapePipe,
     QuizQuestionListComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     QuizRoutingModule
   ]
 })
